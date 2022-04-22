06a9655b15ec808de170ee899806dbb2f79860a0
KIM JINWOO <rlawlsdn263@naver.com>
2022-04-22 20:10:06 +0900

    Momentum: Login Form&Local Storage

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
+const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const loginButton = document.querySelector('#login-form button');
+const greeting = document.querySelector('#greeting');
 
-function onLoginBtnClick() {
-    console.log(loginInput);
-    console.log("hello", loginInput.value);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+//Login Submit
+function onLoginSubmit(e) {
+    e.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+
+    //Saving username in localstorage
+    localStorage.setItem(USERNAME_KEY, username);
+
+    paintGreetings(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show the greeting
+    paintGreetings(savedUsername);
+};
+
+/**
+ * localStorage.setItem('USER_NAME', username);
+ * const userId = localStorage.getItem('USER_NAME');
+ * localStorage.removeItem('USER_NAME');
+ */
