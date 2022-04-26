4ab6695c7fa59376a64254520ff63c7014e399ab
KIM JINWOO <rlawlsdn263@naver.com>
2022-04-26 17:15:14 +0900

    Momentum: Clock

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+const clock = document.getElementById('clock');
+
+//date가 함수 밖에 있으면 고정값이됨;
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock(); //바로 호출하고, 필수!
+setInterval(getClock, 1000); //매초마다 호출
+
+//padStart(num, 'str');
+// const str = "1";
+// console.log(str.padStart(2, '0'));
+
+//String()
